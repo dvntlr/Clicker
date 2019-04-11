caf9ba1a92fc9d62adc47039a500cbbf2ee5b636
dvntlr <49528276+dvntlr@users.noreply.github.com>
2019-04-11 17:30:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var Lemmons = 0;
+var Lemons = 0;
 
-function Lemmonsclick(number) {
-  Lemmons = Lemmons + number;
-  document.getElementById("LemmonsCount").innerHTML = Lemmons
+function Lemonsclick(number) {
+  Lemons = Lemons + number;
+  document.getElementById("LemonsCount").innerHTML = Lemons
 };
 
 var number = 1
@@ -11,18 +11,18 @@ var Trees = 0;
 
 function buytree(){
     var treecost = Math.floor(10 * Math.pow(1.1,Trees));                   //works out the cost of this tree
-    if(Lemmons >= treecost){                                              //checks that the player can afford the tree
+    if(Lemons >= treecost){                                              //checks that the player can afford the tree
         Trees = Trees + 1;                                               //increases number of trees
-    	  Lemmons = Lemmons - treecost;                                   //removes the lemmons spent
+    	  Lemons = Lemons - treecost;                                   //removes the Lemons spent
         document.getElementById('trees').innerHTML = Trees;            //updates the number of trees for the user
-        document.getElementById('LemmonsCount').innerHTML = Lemmons;  //updates the number of lemmons for the user
+        document.getElementById('LemonsCount').innerHTML = Lemons;  //updates the number of Lemons for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,Trees));            //works out the cost of the next tree
     document.getElementById('treecost').innerHTML = nextCost;      //updates the tree cost for the user
 };
 
 window.setInterval (function() {
-  console.log(Lemmons)
-  Lemmonsclick(Trees);
+  console.log(Lemons)
+  Lemonsclick(Trees*10);
 
 }, 1000);
